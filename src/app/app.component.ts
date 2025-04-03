97a8f07c639f0a57f12acc0f49a8bdee1b670dc6
Vedant Malgundkar <vedantmalgundkar6@gmail.com>
2025-04-03 11:58:48 +0530

    copied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../components/header/header.component';
-import { UserComponent } from '../components/user/user.component';
-import { DUMMY_USERS } from './dummy-user';
-import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId?: string;
 
-  onSelectUser($event: any) {
-    console.log($event);
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
   }
 }
